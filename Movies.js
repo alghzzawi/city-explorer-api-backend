2824351073c7cbacc93ae0cf4376aafea914c356
alghzzawi <mohammadalghazzawi9@gmail.com>
2022-09-04 22:19:55 +0300

    store weather and movies data

--- Movies.js
+++ Movies.js
@@ -1,22 +1,33 @@
 const axios = require("axios");
 const ForecastMoviesData = require('./MoviesForecast')
-
+    let myMemory = {}
 function getmoviesHandler(req, res) {
     let {query} = req.query;
-  
-    const URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${query}`
-    axios
-    .get(URL)
-    .then((server)=>{
-      let movieDataArray = server.data.results.map(day=>{
-        return new ForecastMoviesData(day)
+
+    
+    
+    if (myMemory[query] !== undefined)
+    {
+      res.send(myMemory[query])
+      console.log(`I have the movie data`)
+    }
+    else
+    {
+      const URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${query}`
+      axios
+      .get(URL)
+      .then((server)=>{
+        let movieDataArray = server.data.results.map(day=>{
+          return new ForecastMoviesData(day)
+        })
+        myMemory[query] = movieDataArray;
+        res.send(movieDataArray)
+        console.log(`I don't have the movie data`)
       })
-      
-      res.send(movieDataArray)
-    })
-    .catch((error) => {
-      res.status(404).send(error);
-    });
+      .catch((error) => {
+        res.status(404).send(error);
+      });
+    }
   
   }
 
--- WeatherCity.js
+++ WeatherCity.js
@@ -1,23 +1,35 @@
 const axios = require("axios");
 const ForecastWeatherData = require('./WeatherForecast')
 
+let myMemory = {}
 
 function getWeatherHandlerByCity(req, res) {
     let { cityQuery } = req.query;
     
-  
-    const URL = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&city=${cityQuery}`;
-    axios
-      .get(URL)
-      .then((server) => {
-        let weatherArr = server.data.data.map((day) => {
-          return new ForecastWeatherData(day);
+    if (myMemory[cityQuery] !== undefined)
+    {
+      res.send(myMemory[cityQuery])
+      console.log(`I have the wether data`)
+    }
+    else
+    {
+      const URL = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&city=${cityQuery}`;
+      axios
+        .get(URL)
+        .then((server) => {
+          let weatherArr = server.data.data.map((day) => {
+            return new ForecastWeatherData(day);
+          });
+          myMemory[cityQuery] = weatherArr;
+          res.status(200).send(weatherArr);
+          console.log(`I don't have the weather data`)
+        })
+        .catch((error) => {
+          res.status(404).send(error);
         });
-        res.status(200).send(weatherArr);
-      })
-      .catch((error) => {
-        res.status(404).send(error);
-      });
+    }
+  
+
   }
 
 
